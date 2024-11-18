080f85a9547d25f7365759283a95aa217aed44d0
Pankaj Sarkar <pankajsarkarweb456@gmail.com>
2024-11-18 13:22:54 +0530

    one

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 // src/App.tsx
 import React from "react";
-import Timeline from "./components/Timeline";
+// import Timeline from "./components/Timeline";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <h1>Activity Timeline</h1>
-      <Timeline />
+      <h1>Activity Timeline</h1>
+      <h1>Activity Timeline</h1>
+      <h1>Activity Timeline</h1>
+      <h1>Activity Timeline</h1>
+      <h1>Activity Timeline</h1>
+      <h1>Activity Timeline</h1>
+      <h1>Activity Timeline</h1>
+      <h1>Activity Timeline</h1>
+      {/* <Timeline /> */}
+
     </div>
   );
 };
